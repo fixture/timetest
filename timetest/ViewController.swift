e78a880f7dbe2f0d353c9b294300c4993e4f56ed
fixture <wlszfei@163.com>
2020-04-01 11:24:01 +0800

    Initial Commit

---
+++ timetest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timetest
+//
+//  Created by franklin zhang on 4/1/20.
+//  Copyright © 2020 franklin zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
